8dbac70748e196817ee0a0e4de8a3060bb62473f
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-04-03 11:45:08 +0200

    Updates

---
+++ precache-manifest.2df237e549be567bf7ad814cb50b2577.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d2cae5fc88940e7b279b2bc0519d07",
+    "url": "/justfit-client/index.html"
+  },
+  {
+    "revision": "fec562bce38d99416049",
+    "url": "/justfit-client/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "001c8a7bcd9b8ffa9558",
+    "url": "/justfit-client/static/css/main.a6d5efeb.chunk.css"
+  },
+  {
+    "revision": "fec562bce38d99416049",
+    "url": "/justfit-client/static/js/2.cd74259d.chunk.js"
+  },
+  {
+    "revision": "c3b002051c2adeeea342c48f6a949f18",
+    "url": "/justfit-client/static/js/2.cd74259d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001c8a7bcd9b8ffa9558",
+    "url": "/justfit-client/static/js/main.99264967.chunk.js"
+  },
+  {
+    "revision": "117cc08ac1d942e60700",
+    "url": "/justfit-client/static/js/runtime-main.62a0a447.js"
+  },
+  {
+    "revision": "4b279cf199039b525f9169aa19cc0a09",
+    "url": "/justfit-client/static/media/crossfite.4b279cf1.jpg"
+  },
+  {
+    "revision": "c483f47a5fb87ae23528082b36e968cf",
+    "url": "/justfit-client/static/media/gaye.c483f47a.jpg"
+  },
+  {
+    "revision": "fcbad424b926c14d86e0bddb8ffc012b",
+    "url": "/justfit-client/static/media/traininge.fcbad424.jpg"
+  }
+]);
